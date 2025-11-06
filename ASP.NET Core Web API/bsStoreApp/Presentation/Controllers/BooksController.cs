800179df3d6e8b485c25e7b723b4f180c6fecb40
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2025-11-06 11:44:56 +0300

    bsStoreApp Project
    This is a project that covers the logic of RESTful services, HTTP request types, CRUD operations and API development with ASP.NET Core.

---
+++ ASP.NET Core Web API/bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -0,0 +1,94 @@
+﻿using Entities.DataTransferObjects;
+using Entities.Exceptions;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ServiceFilter(typeof(LogFilterAttribute))]
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public BooksController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public async Task<IActionResult> GetAllBooksAsync([FromQuery] BookParameters bookParameters)
+        {
+                var pageResult = await _manager
+                .BookService
+                .GetAllBooksAsync(bookParameters, false);
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(pageResult.metaData));
+                return Ok(pageResult.books);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneBookAsync([FromRoute(Name = "id")] int id)
+        {
+             var book = await _manager
+             .BookService
+             .GetOneBookByIdAsync(id, false);
+                return Ok(book);
+        }
+
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        [HttpPost]
+        public async Task<IActionResult> CreateOneBookAsync([FromBody] BookDtoForInsertion bookDto) //ilgili bilginin reguestin body'sinden geldiğini belli ettik.
+        {
+                var book = await _manager.BookService.CreateOneBookAsync(bookDto);
+                return StatusCode(201, book);  //CreateAtRoute()
+        }
+
+        [ServiceFilter(typeof (ValidationFilterAttribute))]
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateOneBookAsync([FromRoute(Name = "id")] int id,
+            [FromBody] BookDtoForUpdate bookDto)
+        {
+                await _manager.BookService.UpdateOneBookAsync(id, bookDto, false);
+                return NoContent(); //204
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteOneBooksAsync([FromRoute(Name = "id")] int id)
+        {
+              await _manager.BookService.DeleteOneBookAsync(id, false);
+              return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        public async Task<IActionResult> PartiallyUpdateOneBookAsync([FromRoute(Name = "id")] int id,
+            [FromBody] JsonPatchDocument<BookDtoForUpdate> bookPatch)
+        {
+            if(bookPatch is null)
+                return BadRequest();
+
+            var result = await _manager.BookService.GetOneBookForPatchAsync(id, false);
+
+            bookPatch.ApplyTo(result.bookDtoForUpdate, ModelState);
+            TryValidateModel(result.bookDtoForUpdate);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            await _manager.BookService.SaveChangesForPatchAsync(result.bookDtoForUpdate, result.book);
+            return NoContent(); //204
+        }
+    }
+}
---
+++ ASP.NET Core Web API/bsStoreApp/WebApi/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NLog;
+using Presentation.ActionFilters;
+using Repositories.EFCore;
+using Services.Contracts;
+using WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true; //artýk i.erik pazarlýðýna hazýr anlamýna gelir.
+    config.ReturnHttpNotAcceptable = true; //kabul edilmeyen formatý turuya çekerek istediði çýktýyý almasý saðlanýr.
+})
+.AddCustomCsvFormatter()
+.AddXmlDataContractSerializerFormatters()//xml formatýnda çýktý almak için.
+.AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
+.AddNewtonsoftJson();
+
+ 
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigurationLoggerService();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.ConfigureActionFilters();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureDataShaper();
+builder.Services.AddCustomMediaType();
+
+
+var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILoggerService>();
+app.ConfigureExceptionHandler(logger);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+if(app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
